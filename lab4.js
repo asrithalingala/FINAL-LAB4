ad7f042d988d06f6de0ea3bbedb694825d991b29
asrithalingala <alingala@hawk.iit.edu>
2023-11-29 23:27:27 -0600

    modified url of images

--- lab4.js
+++ lab4.js
@@ -64,7 +64,7 @@ const addResultsContainer = (todaysData, tomorrowsData) => {
                     <p class="date">${todayFormateDate}</p>
                     <h5 class="day-type-heading">Sunrise</h5>
                     <img
-                      src="C:\\Users\\asrit\\Downloads\\download (5).png"
+                      src="C:\\Users\\asrit\\OneDrive\\Desktop\\IITC\\FINAL LAB4\\images\\download (5).png"
                       alt="sunImage"
                       class="sun-image"
                     />
@@ -91,7 +91,7 @@ const addResultsContainer = (todaysData, tomorrowsData) => {
                     <p class="date">${tomorrowFormatDate}</p>
                     <h5 class="day-type-heading">Sunrise</h5>
                     <img
-                      src="C:\\Users\\asrit\\Downloads\\download (5).png"
+                      src="C:\\Users\\asrit\\OneDrive\\Desktop\\IITC\\FINAL LAB4\\images\\download (5).png"
                       alt="sunImage"
                       class="sun-image"
                     />
@@ -124,7 +124,7 @@ const addResultsContainer = (todaysData, tomorrowsData) => {
                     <p class="date">${todayFormateDate}</p>
                     <h5 class="day-type-heading">Sunset</h5>
                     <img
-                      src="C:\\Users\\asrit\\Downloads\\download (4).jpg"
+                      src="C:\\Users\\asrit\\OneDrive\\Desktop\\IITC\\FINAL LAB4\\images\\download (4).jpg"
                       alt="sunImage"
                       class="sun-image"
                     />
@@ -153,7 +153,7 @@ const addResultsContainer = (todaysData, tomorrowsData) => {
                     <p class="date">${tomorrowFormatDate}</p>
                     <h5 class="day-type-heading">Sunset</h5>
                     <img
-                      src="C:\\Users\\asrit\\Downloads\\download (4).jpg"
+                      src="C:\\Users\\asrit\\OneDrive\\Desktop\\IITC\\FINAL LAB4\\images\\download (4).jpg"
                       alt="sunImage"
                       class="sun-image"
                     />
